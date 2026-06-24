52ae267c2cc5d090bd88e7c8bd3e3cf645786935
AndreyMescheryakov <sunandrey2005@gmail.com>
2026-06-24 08:26:50 +0300

    Add input validation, error handling and Swagger auth button

--- src/main/java/com/vsu/demo/DemoApplication.java
+++ src/main/java/com/vsu/demo/DemoApplication.java
@@ -1,9 +1,15 @@
 package com.vsu.demo;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@SecurityScheme(name = "basicAuth", type = SecuritySchemeType.HTTP, scheme = "basic")
+@OpenAPIDefinition(security = @SecurityRequirement(name = "basicAuth"))
 public class DemoApplication {
 
 	public static void main(String[] args) {
